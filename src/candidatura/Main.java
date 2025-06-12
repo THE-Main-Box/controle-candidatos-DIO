b18c20d4bcc055fa58c90d298e49d02d928c1706
Thiago Barbosa <curiosidades381@gmail.com>
2025-06-12 17:47:43 -0300

    REFACT
    *Mudança da localização da inicialização do sistema
    (Agora a classe de processo seletivo possue apenas os métodos para lidar com o processo seletivo)
    
    *Adição do salário desejado e do salário base
    (Foram adicionadas para realizar debug e leitura dos valores)

---
+++ src/candidatura/Main.java
@@ -0,0 +1,12 @@
+package candidatura;
+
+import candidatura.util.SelectiveProcess;
+
+public class Main {
+    public static void main(String[] args) {
+        SelectiveProcess SP = new SelectiveProcess();
+
+        SP.checkCandidate(5500, 1);
+
+    }
+}
